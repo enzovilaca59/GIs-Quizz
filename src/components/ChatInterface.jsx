02a6361392fc9b2cd13c66c45f5c717593547b95
enzovilaca59 <vilacaenzo59@gmail.com>
2025-10-31 08:59:44 +0100

    leDernierJeVeuxRienSavoir

--- src/components/ChatInterface.jsx
+++ src/components/ChatInterface.jsx
@@ -203,7 +203,7 @@ const styles = `
   height: 800px;
   display: flex;
   flex-direction: column;
-  
+
 }
 
 .chat-header {
@@ -227,7 +227,7 @@ const styles = `
   flex: 1;
   overflow-y: auto;
   padding: 20px;
-  background-image: url("../version finaleV2 askip.png");
+  background-image: url("../../public/version finaleV2 askip.png");
 }
 
 .message {
